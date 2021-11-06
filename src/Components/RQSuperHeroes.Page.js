873fe86fe8ab00adda6fb75d260a78c4b689baeb
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-06 11:21:17 +0530

    fetching data using react query

--- src/Components/RQSuperHeroes.Page.js
+++ src/Components/RQSuperHeroes.Page.js
@@ -1,11 +1,13 @@
 import {useQuery} from 'react-query';
 import axios from 'axios';
 
+const fetchSuperHeroes = () => {
+    return axios.get('http://localhost:3000/superheroes')
+}
+
 const RQSuperHeroesPage = () => {
 
-    const {isLoading, data } = useQuery('super-heroes', ()=>{
-        return axios.get('http://localhost:3000/superheroes')
-    })
+    const {isLoading, data } = useQuery('super-heroes', fetchSuperHeroes)
 
     if(isLoading){
         return <h2>Loading...</h2>
