25aac9c10d8f39f90e433a48d1b6bef68cb5df52
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-07 14:54:53 +0530

    data fetching using parallel query

--- src/Components/ParallelQuery.Page.js
+++ src/Components/ParallelQuery.Page.js
@@ -1,4 +1,19 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchSuperHeroes = () => {
+    return axios.get(`http://localhost:3000/superheroes`)
+}
+
+const fetchFriends = () => {
+    return axios.get(`http://localhost:3000/friends`)
+}
+
 const ParallelQueryPage = () => {
+
+    const {data: superHeroes} = useQuery('super-heroes', fetchSuperHeroes)
+    const {data: friends} = useQuery('friends', fetchFriends) 
+
     return <div>ParallelQueryPage</div>
 }
 export default ParallelQueryPage
