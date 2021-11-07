f2da7dfcf79c8de4a2d6dbf656ce16b7edff246f
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-07 12:17:54 +0530

    fetch superHero by id and displaying the details

--- src/Components/RQSuperHero.Page.js
+++ src/Components/RQSuperHero.Page.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useSuperHeroData } from '../hooks/useSuperHeroData'
 const RQSuperHeroPage = () => {
+
+    const  {heroId} = useParams()
+    const {isLoading, data, isError, error} = useSuperHeroData(heroId)
+
+    if(isLoading){
+        return <h2>Loading...</h2>
+    }
+
+    if(isError){
+        return <h2>{error.message}</h2>
+    }
+
     return (
         <div>
-            Super hero details
+            {data?.data.name} - {data?.data.alterEgo}
         </div>
     )
 }
--- src/hooks/useSuperHeroData.js
+++ src/hooks/useSuperHeroData.js
@@ -1,6 +1,10 @@
 import { useQuery } from "react-query";
 import axios from "axios";
 
-export const useSuperHeroData = (heroId) => {
+const fetchSuperHero = (heroId) => {
+    return axios.get(` http://localhost:3000/superheroes/${heroId}`)
+}
 
+export const useSuperHeroData = (heroId) => {
+    return useQuery(['super-hero', heroId], () => fetchSuperHero(heroId))
 }
